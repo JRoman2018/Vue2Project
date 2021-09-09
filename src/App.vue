0371d7be62bd078b16dd289224b83409d8461476
jose roman <el-1ero@outlook.com>
2021-09-08 21:02:42 -0400

    routes created

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <app-header></app-header>
+    <div class="row">
+      <div class="col-md-12">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import Header from './components/Header.vue';
+export default {
+  components: { appHeader: Header },
+};
 </script>
 
-<style></style>
+<style>
+body {
+  padding: 30px;
+}
+</style>
